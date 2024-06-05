b29a0bac0d1de63a40761dd38520a9b99d431a78
YuriyEG <you@example.com>
2024-06-05 22:52:28 +1200

    bug fix, getCurrentTime helper

--- src/helper/getCurrentTime.ts
+++ src/helper/getCurrentTime.ts
@@ -1,5 +1,5 @@
 const getCurrentTime = () => {
-  return new Date(Date.now() + new Date().getTimezoneOffset() * 60000)
+  return new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
     .toISOString()
     .substr(11, 8)
 }
